7c2da2346904c6112fecd293f4c577d0c7552a7c
Mark Galea <mark.galea@cloudreach.com>
2012-06-27 01:40:57 +0100

    Updated code for the ngram generation.

--- scripts/get_top_ngrams.sh
+++ scripts/get_top_ngrams.sh
@@ -1,18 +1,41 @@
+#!/bin/bash
+
+# Retrieve the dictionary from the text.  
+function getDict {
+	declare -a language_model
+	TEXT=$1
+	NGRAMS=$2
+	LIMIT=$3
+	LANGUAGE_MODEL=$(echo $TEXT | \
+		awk '{ print tolower($0) }' | \
+		tr -Cd 'a-zA-Z' | \
+		awk -v ngram=$NGRAMS '{for(i = 1; i <= length($0) - (ngram-1); i++)  print substr($0, i, ngram)}' | \
+		sort | \
+		uniq -ci | \
+		sort -n -k 1 -r | \
+		awk 'BEGIN {i=0; previous=-1} {if ($1 != previous) {previous = $1; i++}} {print i, $0}' | \
+		awk -v limit=$LIMIT '($1<=limit) { print $1,$0 }') 
+
+	
+	printf %s "$LANGUAGE_MODEL" | while IFS= read -r line
+	do
+		index=`echo "$line" | awk '{print $1}'`
+		ngram=`echo "$line" | awk '{print $4}'`
+		echo "[$line] $index $ngram"
+	done
+}
+
+
 export LC_CTYPE=C
 
-if [  $# -ne 3 ]
+if [  $# -ne 4 ]
 then
-	echo "Usage: <File> <Ngrams> <Limit>"
+	echo "Usage: <Dataset> <Text> <Ngrams> <Limit>"
 	exit 0
 fi
+DATASET=`cat $1`
+echo "Loading Dataset [$1]"
+getDict "$DATASET" $3 $4
 
 
-cat $1 | \
-	awk '{ print tolower($0) }' | \
-	tr -Cd 'a-zA-Z' | \
-	awk -v ngram=$2 '{for(i = 1; i <= length($0) - (ngram-1); i++)  print substr($0, i, ngram)}' | \
-	sort | \
-	uniq -ci | \
-	sort -n -k 1 -r | \
-	awk 'BEGIN {i=0; previous=-1} {if ($1 != previous) {previous = $1; i++}} {print i, $0}' | \
-	awk -v limit=$3 '($1<=limit) { print $1,$0 }' 
+# getDict $2 $3 $4
